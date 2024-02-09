70d20834d44bf6030d11fb99e6645cfdbc83d48a
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-09 03:54:07 +0200

    Implemented routing to different component's

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Routes, Route } from "react-router-dom";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <HomePage />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="/search" element={<SearchBooksPage />} />
+          <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
