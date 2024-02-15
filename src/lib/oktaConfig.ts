698ba24a03b141ca6f2c650afb01198fc266f6eb
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-15 12:44:25 +0200

    Adding support for https instead of http

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: "0oaf21im7p8CaoLyc5d7",
   issuer: "https://dev-44355015.okta.com/oauth2/default",
-  redirectUri: "http://localhost:3000/login/callback",
+  redirectUri: "https://localhost:3000/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
