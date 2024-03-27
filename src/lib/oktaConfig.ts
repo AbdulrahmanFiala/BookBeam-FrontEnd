273a417ab508be15915e6e50b873a35eabc1352f
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-27 06:48:41 +0200

    Removing sensitive data from the oktaConfig file

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-  clientId: "0oaf21im7p8CaoLyc5d7",
-  issuer: "https://dev-44355015.okta.com/oauth2/default",
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  issuer: process.env.REACT_APP_OKTA_ISSUER,
   redirectUri: "https://bookbeam.vercel.app/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
